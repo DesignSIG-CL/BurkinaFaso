5e24e4a4a7cdd23a59fea862642eca1b6cfb3493
ZB-BEN <benoit.corday@gmail.com>
2017-11-19 19:04:38 +0100

    V0.40 Interaction interact with the inter-actor
    Some improvement, really better than the version from v0.39

--- BFwebSIG_v3/public/javascripts/mapEngine.js
+++ BFwebSIG_v3/public/javascripts/mapEngine.js
@@ -13,7 +13,8 @@ var regionsStyle = new ol.style.Style({
 
 var vectorlayerroad = new ol.layer.Vector();
 var vectorlayerregion = new ol.layer.Vector();
-var vectorAdd = new ol.layer.Vector();
+var vector = new ol.layer.Vector();
+var source = new ol.layer.Vector();
 
 $(document).ready(function(){
   var map = new ol.Map({
@@ -44,134 +45,70 @@ $(document).ready(function(){
   });
 
 // Definition of the layer for the interaction
-  var vectorAdd = new ol.layer.Vector({
-        source: new ol.source.Vector(),
+  var source = new ol.source.Vector();
+  var vector = new ol.layer.Vector({
+        source: source,
         style: new ol.style.Style({
           fill: new ol.style.Fill({
             color: 'rgba(255, 255, 255, 0.2)'
           }),
           stroke: new ol.style.Stroke({
-            color: 'rgba(100, 0, 100, 1)',
+            color: '#ffcc33',
             width: 2
           }),
           image: new ol.style.Circle({
             radius: 7,
             fill: new ol.style.Fill({
-              color: 'rgba(100, 0, 100, 1)'
+              color: '#ffcc33'
             })
           })
         })
-  });
+      });
+
 
   map.addLayer(osmlayer);
   map.addLayer(vectorlayerroad);
   map.addLayer(vectorlayerregion);
-  map.addLayer(vectorAdd);
+  map.addLayer(vector);
 
   // Adding new vector elements on the map.
-  var Modify = {
-          init: function() {
-            this.select = new ol.interaction.Select();
-            map.addInteraction(this.select);
+  var modify = new ol.interaction.Modify({source: source});
+      map.addInteraction(modify);
 
-            this.modify = new ol.interaction.Modify({
-              features: this.select.getFeatures()
-            });
-            map.addInteraction(this.modify);
+      var draw, snap; // global so we can remove them later
+      var typeSelect = document.getElementById('draw-type');
 
-            this.setEvents();
-          },
-          setEvents: function() {
-            var selectedFeatures = this.select.getFeatures();
-
-            this.select.on('change:active', function() {
-              selectedFeatures.forEach(selectedFeatures.remove, selectedFeatures);
-            });
-          },
-          setActive: function(active) {
-            this.select.setActive(active);
-            this.modify.setActive(active);
-          }
-        };
-        Modify.init();
-
-        var optionsForm = document.getElementById('options-form');
-
-        var Draw = {
-          init: function() {
-            map.addInteraction(this.Point);
-            this.Point.setActive(false);
-            map.addInteraction(this.LineString);
-            this.LineString.setActive(false);
-            map.addInteraction(this.Polygon);
-            this.Polygon.setActive(false);
-            map.addInteraction(this.Circle);
-            this.Circle.setActive(false);
-          },
-          Point: new ol.interaction.Draw({
-            source: vectorAdd.getSource(),
-            type: /** @type {ol.geom.GeometryType} */ ('Point')
-          }),
-
-          LineString: new ol.interaction.Draw({
-            source: vectorAdd.getSource(),
-            type: /** @type {ol.geom.GeometryType} */ ('LineString')
-          }),
-          Polygon: new ol.interaction.Draw({
-            source: vectorAdd.getSource(),
-            type: /** @type {ol.geom.GeometryType} */ ('Polygon')
-          }),
-          Circle: new ol.interaction.Draw({
-            source: vectorAdd.getSource(),
-            type: /** @type {ol.geom.GeometryType} */ ('Circle')
-          }),
-          getActive: function() {
-            return this.activeType ? this[this.activeType].getActive() : false;
-          },
-          setActive: function(active) {
-            var type = optionsForm.elements['draw-type'].value;
-            if (active) {
-              this.activeType && this[this.activeType].setActive(false);
-              this[type].setActive(true);
-              this.activeType = type;
-            } else {
-              this.activeType && this[this.activeType].setActive(false);
-              this.activeType = null;
-            }
-          }
-        };
-        Draw.init();
-
-        /**
-         * Let user change the geometry type.
-         * @param {Event} e Change event.
-         */
-        optionsForm.onchange = function(e) {
-          var type = e.target.getAttribute('name');
-          var value = e.target.value;
-          if (type == 'draw-type') {
-            Draw.getActive() && Draw.setActive(true);
-          } else if (type == 'interaction') {
-            if (value == 'modify') {
-              Draw.setActive(false);
-              Modify.setActive(true);
-            } else if (value == 'draw') {
-              Draw.setActive(true);
-              Modify.setActive(false);
-            }
-          }
-        };
-
-        Draw.setActive(true);
-        Modify.setActive(false);
-
-        // The snap interaction must be added after the Modify and Draw interactions
-        // in order for its map browser event handlers to be fired first. Its handlers
-        // are responsible of doing the snapping.
-        var snap = new ol.interaction.Snap({
-          source: vectorAdd.getSource()
+      function addInteractions() {
+        draw = new ol.interaction.Draw({
+          source: source,
+          type: /** @type {ol.geom.GeometryType} */ (typeSelect.value)
         });
+        // Adding an event at the end of the draw.
+        draw.on('drawend', function(evt){
+          console.log('And one draw appears');
+          //in evt you will get ol.feature
+          // from ol.feature get the geometry and than get coordinates
+          var coord = evt.feature.getGeometry().getCoordinates();
+          document.getElementById("OurInteraction").style.visibility="visible";
+          document.getElementById("OurInput").innerHTML = coord.toString() ;
+        });
+        // End of the event
+        map.addInteraction(draw);
+        snap = new ol.interaction.Snap({source: source});
         map.addInteraction(snap);
+
+      }
+
+      /**
+       * Handle change event.
+       */
+      typeSelect.onchange = function() {
+        map.removeInteraction(draw);
+        map.removeInteraction(snap);
+        addInteractions();
+      };
+
+      addInteractions();
 
 });
 
@@ -180,6 +117,15 @@ $(document).ready(function(){
 function setVisibleLayers(){
   vectorlayerroad.setVisible(document.getElementById("roadlinesCheck").checked);
   vectorlayerregion.setVisible(document.getElementById("regionsCheck").checked);
-  vectorAdd.setVisible(document.getElementById("interactionsCheck").checked); // Doesn't work
+  source.setVisible(document.getElementById("interactionsCheck").checked); // Doesn't work
   console.log('Changing the layers visibility.');
+}
+
+function cancelAndCloseInterraction(){
+  document.getElementById("OurInteraction").style.visibility="hidden";
+}
+
+function saveAndCloseInterraction(){
+  alert("I'm not implemented.");
+  document.getElementById("OurInteraction").style.visibility="hidden";
 }
