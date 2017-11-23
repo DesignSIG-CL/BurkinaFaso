6f3a9353771966c29939489734a4c8589bdf8954
AlphonseIrtu <rene.lugrin@epfl.ch>
2017-11-23 18:15:01 +0100

    new version of interaction start
    voillllllaaaaaaaaaaaaa

--- BFwebSIG_v3/public/javascripts/mapEngine.js
+++ BFwebSIG_v3/public/javascripts/mapEngine.js
@@ -9,17 +9,111 @@ var regionsStyle = new ol.style.Style({
       fill: new ol.style.Fill({ color: 'rgba(100, 0, 0, 0.2)', width: 4 }),
       stroke: new ol.style.Stroke({ color: 'rgba(100,0,0,1)', width: 1 })
 });
-
-// This initial defintion allows to use the layers out of the jQuery
+var map;
 var vectorlayerroad = new ol.layer.Vector();
 var vectorlayerregion = new ol.layer.Vector();
+
+
+
+function init() {
+// Crée la carte Lat/Lon avec une couche de fond OpenStreetMap
+
+map = new ol.Map({
+  layers: [
+    new ol.layer.Tile({
+      source: new ol.source.OSM()
+  })
+  ],
+  target: 'OurMap',
+  renderer: 'canvas',
+  view: new ol.View({
+    center: ol.proj.transform([-2, 12.1], 'EPSG:4326', 'EPSG:3857'),
+    zoom: 7
+  })
+});
+map.addLayer(vectorlayerroad);
+map.addLayer(vectorlayerregion);
+
+document.getElementById("addButton").onclick = setMode;
+document.getElementById("modButton").onclick = setMode;
+document.getElementById("delButton").onclick = setMode;
+
+}
+
+vectorlayerregion = new ol.layer.Vector({
+    style: regionsStyle,
+    source: new ol.source.Vector({
+      url: '/images/geoData/limiteadminpolygon.geojson',
+      format: new ol.format.GeoJSON(),
+    })
+  });
+
+vectorlayerroad = new ol.layer.Vector({
+    style: roadlinesStyle,
+    source: new ol.source.Vector({
+      url: '/images/geoData/roadlines.geojson',
+      format: new ol.format.GeoJSON()
+    })
+  });
+
+var RoadStyle = new ol.style.Style({
+  stroke: new ol.style.Stroke({ color: 'rgba(50,100,0,1)', width:1.5})
+});
+
+var RoadSource = new ol.source.Vector();
+
+var RoadLayer = new ol.layer.Vector({
+  style: RoadStyle,
+  source: RoadSource
+});
+// ajouter les couches Pistes, Ouvrages, Ponts
+
+var mode = "none";
+
+var draw = new ol.interaction.Draw({
+  source: RoadSource,
+  type: "LineString"
+  });
+var snap = new ol.interaction.Snap({source: RoadSource});
+
+//var modifier à ajouter et delete
+
+function setMode() {
+  console.log(this.id);
+
+  if(this.id == "addButton"){
+      console.log('jysouis dans this ID 7 moawde yeah');
+
+    if(mode == "add"){
+      mode = "none";
+      this.style.color = "black";
+    }
+    else {
+      console.log('en mode add lard enfin');
+
+      mode = "add";
+      this.style.color = "red";
+      draw.on('drawend', function(evt) {RoadAdded(evt)} );
+      map.addInteraction(draw);
+      map.addInteraction(snap);
+    }
+  }
+  else if(this.id == "modButton") {
+  }
+  else if(this.id == "delButton") {
+  }
+};
+
+
+// This initial defintion allows to use the layers out of the jQuery
+
 var vector = new ol.layer.Vector();
-var source = new ol.layer.Vector();
+var source = new ol.source.Vector();
 
 // jQuery : This part is exectuted after the initialisation of the page
 // It's necessary to allow cross origin references
-$(document).ready(function(){
-  var map = new ol.Map({
+//$(document).ready(function(){
+/*  var map = new ol.Map({
     target: 'OurMap',
     view: new ol.View({
       center: ol.proj.fromLonLat([-2,12.1]),
@@ -29,22 +123,8 @@ $(document).ready(function(){
   var osmlayer = new ol.layer.Tile({
     source: new ol.source.OSM()
   });
-
-  vectorlayerregion = new ol.layer.Vector({
-    style: regionsStyle,
-    source: new ol.source.Vector({
-      url: '/images/geoData/limiteadminpolygon.geojson',
-      format: new ol.format.GeoJSON(),
-    })
-  });
-
-  vectorlayerroad = new ol.layer.Vector({
-    style: roadlinesStyle,
-    source: new ol.source.Vector({
-      url: '/images/geoData/roadlines.geojson',
-      format: new ol.format.GeoJSON()
-    })
-  });
+*/
+  
 
 // Definition of the layer for the interaction
   var source = new ol.source.Vector();
@@ -67,10 +147,8 @@ $(document).ready(function(){
         })
       });
 
-
+/*
   map.addLayer(osmlayer);
-  map.addLayer(vectorlayerroad);
-  map.addLayer(vectorlayerregion);
   map.addLayer(vector);
 
   // Adding new vector elements on the map.
@@ -83,20 +161,20 @@ $(document).ready(function(){
       function addInteractions() {
         draw = new ol.interaction.Draw({
           source: source,
-          type: /** @type {ol.geom.GeometryType} */ (typeSelect.value)
-        });
+     ////     type: /** @type {ol.geom.GeometryType} */ ////(typeSelect.value)
+     ////   });
 
         // Adding an event at the end of the draw.
-        draw.on('drawend', function(evt){
+        function RoadAdded(evt) {
           console.log('And a new draw appears');
           //in evt you will get ol.feature
           // from ol.feature get the geometry and than get coordinates
           var coord = evt.feature.getGeometry().getCoordinates();
           document.getElementById("OurInteraction").style.visibility="visible";
           document.getElementById("OurInput").innerHTML = coord.toString() ;
-        });
+        };
         // End of the event
-        map.addInteraction(draw);
+        /*map.addInteraction(draw);
         snap = new ol.interaction.Snap({source: source});
         map.addInteraction(snap);
 
@@ -105,18 +183,18 @@ $(document).ready(function(){
       /**
        * Handle change event.
        */
-      typeSelect.onchange = function() {
-        map.removeInteraction(draw);
-        map.removeInteraction(snap);
-        addInteractions();
-      };
+//      typeSelect.onchange = function() {
+//        map.removeInteraction(draw);
+//        map.removeInteraction(snap);
+//        addInteractions();
+//      };
 
-      addInteractions();
+//      addInteractions();
 
-});
+//});
 
 // Setting the visible layers
-
+/*
 function setVisibleLayers(){
   vectorlayerroad.setVisible(document.getElementById("roadlinesCheck").checked);
   vectorlayerregion.setVisible(document.getElementById("regionsCheck").checked);
@@ -140,4 +218,4 @@ function saveAndCloseInterraction(){
   alert("Les données que vous avez saisies ne sont pas enregistrées pour le moment ! Elles sont dans la console (F12).");
   console.log(text);
   x.reset();
-}
+}*/
