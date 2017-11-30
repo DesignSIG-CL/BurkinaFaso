cc24eb1724b831c6c7087246a6efb03f90bbc91a
ZB-BEN <benoit.corday@gmail.com>
2017-11-30 17:49:44 +0100

    V0.44 MongoDB is working

--- BFwebSIG_v3/routes/map.js
+++ BFwebSIG_v3/routes/map.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET map's page. */
 router.get('/', function(req, res, next) {
+  console.log('Loading the webpage with the map.');
   res.render('map', { title: 'WEBSIG B-F' });
 });
 
