12c245225cf76232b3c2316ebf6d90dddeb5ee6e
ZB-BEN <benoit.corday@gmail.com>
2017-12-14 10:24:32 +0100

    V0.53 Moving the styles in a separate file.
    - This is to fit a little bit the mapEngine, who's the main javascript file on the client side.

--- BFwebSIG_v3/public/javascripts/mapEngine.js
+++ BFwebSIG_v3/public/javascripts/mapEngine.js
@@ -1,32 +1,5 @@
 console.log('mapEngine is running');
 
-// Styles of the GeoJSON static layers
-var roadlinesStyle = new ol.style.Style({
-    stroke: new ol.style.Stroke({ color: 'rgba(0,100,100,1)', width: 2.0 })
-});
-
-var regionsStyle = new ol.style.Style({
-      fill: new ol.style.Fill({ color: 'rgba(100, 0, 0, 0.0)', width: 4 }),
-      stroke: new ol.style.Stroke({ color: 'rgba(100,0,0,1)', width: 1 })
-});
-
-var observationsStyle = new ol.style.Style({
-    stroke: new ol.style.Stroke({ color: 'rgba(100,100,0,1)', width: 3.0 })
-});
-
-// Styles for pimpable data
-var routesStyle = new ol.style.Style({
-    stroke: new ol.style.Stroke({ color: 'rgba(0,100,100,1)', width: 2.0 })
-});
-
-var pistesStyle = new ol.style.Style({
-    stroke: new ol.style.Stroke({ color: 'rgba(0,100,100,1)', width: 2.0 })
-});
-
-var ouvragesStyle = new ol.style.Style({
-    stroke: new ol.style.Stroke({ color: 'rgba(0,100,100,1)', width: 2.0 })
-});
-
 var map;
 
 // Function to load data from the pimpable layers
