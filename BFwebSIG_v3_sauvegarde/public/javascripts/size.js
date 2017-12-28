d1ceb81c3b91862e9fc75af0759c1aa02204f1c5
AlphonseIrtu <rene.lugrin@epfl.ch>
2017-12-28 15:12:13 +0100

    inutile

--- BFwebSIG_v3_sauvegarde/public/javascripts/size.js
+++
@@ -1,8 +0,0 @@
-function size() {
-    var h = ""
-    h += window.innerHeight-160;
-    h +="px"
-    document.getElementById("content").style.height = h;
-    document.getElementById("OurMap").style.height = h;
-    // document.getElementById("OurLegend").style.height = h;
-}
