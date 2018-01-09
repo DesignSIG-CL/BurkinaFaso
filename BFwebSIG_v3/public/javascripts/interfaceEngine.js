a64093323fc3cca8126f942bd601ea70b1131055
AlphonseIrtu <rene.lugrin@epfl.ch>
2018-01-09 11:42:23 +0100

    enfin fonctionner
    hidden pas fonctionner,
    collapse pas fonctionner,
    block et display none fonctionner

--- BFwebSIG_v3/public/javascripts/interfaceEngine.js
+++ BFwebSIG_v3/public/javascripts/interfaceEngine.js
@@ -4,7 +4,7 @@ var etatEditor = 0;
 function menu() {
     if (etatLegend==0) {
       // We want to display the legend
-      document.getElementById("OurLegend").style.visibility="visible";
+      document.getElementById("OurLegend").style.display="block";
       document.getElementById("OurButtonLegend").style.backgroundColor="rgb(0,100,100)";
       document.getElementById("OurInteraction").style.top = '31em';
       console.log('Display menu')
@@ -12,7 +12,7 @@ function menu() {
 
     } else {
       // We want to hide the legend
-      document.getElementById("OurLegend").style.visibility="collapse";
+      document.getElementById("OurLegend").style.display="none";
       document.getElementById("OurButtonLegend").style.backgroundColor="rgb(100,0,0)";
       document.getElementById("OurInteraction").style.top = '10.5em';
       console.log('Hide menu')
@@ -23,14 +23,14 @@ function menu() {
 function edit() {
     if (etatEditor==0) {
       // We want to display the editor options
-      document.getElementById("EditorOption").style.visibility="visible";
+      document.getElementById("EditorOption").style.display="block";
       document.getElementById("OurButtonEdit").style.backgroundColor="rgb(0,100,100)";
       console.log('Display editor')
       etatEditor = 1;
 
     } else {
       // We want to hide the editor options
-      document.getElementById("EditorOption").style.visibility="collapse";
+      document.getElementById("EditorOption").style.display="none";
       document.getElementById("OurButtonEdit").style.backgroundColor="rgb(100,0,0)";
       console.log('Hide editor')
       etatEditor = 0;
