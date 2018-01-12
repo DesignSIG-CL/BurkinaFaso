6567b401a1515051ce3ce30b3a6114438e28513e
ZB-BEN <benoit.corday@gmail.com>
2018-01-12 12:04:45 +0100

    V1.00 RENDU propre

--- Essai/website_CORDAY/js/size.js
+++
@@ -1,8 +0,0 @@
-function size() {
-    var h = ""
-    h += window.innerHeight-160;
-    h +="px"
-    document.getElementById("content").style.height = h;
-    document.getElementById("map").style.height = h;
-    document.getElementById("menuOption").style.height = h;
-}
